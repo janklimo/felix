fe6007b68627bdaa0dd5078611a6973ec3dac143
Jan Klimo <jan.klimo@gmail.com>
2016-12-22 23:06:17 +0100

    add navbar and custom controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations',
+    confirmations: 'admins/confirmations',
+    passwords: 'admins/passwords'
+  }
   root to: "home#index"
 end
