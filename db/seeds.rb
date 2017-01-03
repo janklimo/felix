ebb9440f9c7ac436d2880c4bc9ecc84fe2792ca0
Jan Klimo <jan.klimo@gmail.com>
2017-01-03 22:52:21 +0700

    fix question length

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@
 general = Metric.create(name: 'General', image_url: 'https://s3.amazonaws.com/felixthebot/general_cover.png')
 
 # Question #1
-welcome = Question.create(title: 'What do you think about Felix? Is tracking ' \
-                'how happy we are at work important?', metric: general)
+welcome = Question.create(title: 'Is tracking how happy we are at work important?',
+                          metric: general)
 welcome.options << Option.create(title: "It's stupid", value: 0)
 welcome.options << Option.create(title: "I've seen worse", value: 33)
 welcome.options << Option.create(title: "I like it", value: 66)
